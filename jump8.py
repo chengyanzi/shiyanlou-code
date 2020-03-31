55396e7d465fd290382a921b27a0d1d60d5ad0b0
chengyanzi <chrpy_817@126.com>
2020-03-31 21:24:19 +0800

    first commit

---
+++ jump8.py
@@ -0,0 +1,21 @@
+Code/                                                                                               0000775 0011610 0011610 00000000000 13640405460 012534  5                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              Code/jump7.py                                                                                       0000664 0011610 0011610 00000000220 13640405460 014142  0                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              a=0
+while a<=100:
+    a +=1
+    if a%7==0:
+        continue
+    elif a%10==7:
+        continue
+    elif a//10==7:
+        continue
+    print(a)
+                                                                                                                                                                                                                                                                                                                                                                                Code/junp7.py                                                                                       0000664 0011610 0011610 00000000220 13640405201 014134  0                                                                                                    ustar   shiyanlou                       shiyanlou                                                                                                                                                                                                              a=0
+while a<=100:
+    a +=1
+    if a%7==0:
+        continue
+    elif a%10==7:
+        continue
+    elif a//10==7:
+        continue
+    print(a)
+                                                                                                                                                                                     
